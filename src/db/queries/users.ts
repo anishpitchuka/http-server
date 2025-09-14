b0446476e6412d4ce4feaf5232e1501151e9cc09
anishpitchuka <anishpitchuka8@gmail.com>
2025-09-14 18:54:28 +0000

    implement create user

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -1,12 +1,15 @@
-import { db } from "..";
-import { NewUser, users } from "../schema";
+import { db } from "../index.js";
+import { NewUser, users } from "../schema.js";
 
 export async function createUser(user: NewUser) {
-    const [result] = await db
-        .insert(users)
-        .values(user)
-        .onConflictDoNothing()
-        .returning();
-    
-    return result;
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning();
+  return result;
+}
+
+export async function reset() {
+  await db.delete(users);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,30 @@
 import express from "express";
-
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
 import { errorMiddleware, middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerChirpsValidate } from "./api/validate_chirp.js";
-import postgres from "postgres";
 import { config } from "./config.js";
-import {migrate} from "drizzle-orm/postgres-js/migrator.js"
-import { drizzle } from "drizzle-orm/postgres-js/driver.js";
+import { handlerUsersCreate } from "./api/users.js";
 
-
-const migrationClient = postgres(config.db.url, {max: 1});
+const migrationClient = postgres(config.db.url, { max: 1 });
 await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app = express();
 
 app.use(middlewareLogResponse);
-app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use(express.json());
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/api/healthz",  (req, res, next) => {
+app.get("/api/healthz", (req, res, next) => {
   Promise.resolve(handlerReadiness(req, res)).catch(next);
 });
-
 app.get("/admin/metrics", (req, res, next) => {
   Promise.resolve(handlerMetrics(req, res)).catch(next);
 });
-
 app.post("/admin/reset", (req, res, next) => {
   Promise.resolve(handlerReset(req, res)).catch(next);
 });
@@ -36,9 +33,12 @@ app.post("/api/validate_chirp", (req, res, next) => {
   Promise.resolve(handlerChirpsValidate(req, res)).catch(next);
 });
 
+app.post("/api/users", (req, res, next) => {
+  Promise.resolve(handlerUsersCreate(req, res)).catch(next);
+});
+
 app.use(errorMiddleware);
 
 app.listen(config.api.port, () => {
   console.log(`Server is running at http://localhost:${config.api.port}`);
 });
-
