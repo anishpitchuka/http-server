a62ac1262f99212371ec771e86ee1ada90c5a35f
anishpitchuka <anishpitchuka8@gmail.com>
2025-09-14 12:42:08 +0000

    implement JSON handling & parsing

---
+++ src/api/validate_chirp.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { respondWithError, respondWithJSON } from "./json.js";
+
+
+export function handlerChirpsValidate(req: Request, res: Response) {
+    type parameters = {
+        body: string;
+    };
+
+    const params: parameters = req.body;
+    const maxChirpLength = 140;
+    if(params.body.length > maxChirpLength) {
+        respondWithError(res, 400, "Chirp is too long");
+        return;
+    }
+
+
+    const words = params.body.split(" ");
+    const badWords = ["kerfuffle", "sharbert", "fornax"];
+    for(let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const loweredWord = word.toLowerCase();
+        if(badWords.includes(loweredWord)) {
+            words[i] = '****';
+        }
+    }
+    const cleaned = words.join(" ");
+
+    respondWithJSON(res, 200, {cleanedBody: cleaned});
+
+}
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,19 @@ import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
+import { handlerChirpsValidate } from "./api/validate_chirp.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+app.use(express.json());
 
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
